f8d4529be403eccc43049d713e9a4ac2e79bf279
trontzal <trontzal@gmail.com>
2023-10-02 15:33:55 +0200

    quito fon awesome para el deploy

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import router from './router'
 import store from './store'
 import '@/assets/styles/globales.scss'
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { faLinkedin, faGithub, faHtml5, faCss3, faJs } from '@fortawesome/free-brands-svg-icons';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+// import { faLinkedin, faGithub, faHtml5, faCss3, faJs } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faLinkedin,faGithub,faHtml5,faCss3,faJs);
+// library.add(faLinkedin, faGithub, faHtml5, faCss3, faJs);
 
 
-createApp(App).use(store).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
 
