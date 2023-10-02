13df1d45ef7753a8ace3b4099ed124ee3c22ebe2
trontzal <trontzal@gmail.com>
2023-10-02 14:21:04 +0200

    responsive

--- src/components/SobreMi/datosPersonales.vue
+++ src/components/SobreMi/datosPersonales.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="dentroCuadrado flexDatosPersonales">
         <div class="tituloPequeno"><h3>DATOS PERSONALES</h3></div>
-        <div class="grid linea">
+        <div class="grid linea marginTablet">
             <p>Nombre:</p>
             <p>Gonzalo</p>
         </div>
-        <div class="grid linea">
+        <div class="grid linea marginTablet">
             <p>Apellidos:</p>
             <p>Lecumberri Bergareche</p>
         </div>
-        <div class="grid linea">
+        <div class="grid linea marginTablet">
             <p>Linkedin:</p>
             <div class="linkedin"><a href="https://www.linkedin.com/in/gonzalo-lecumberri-496686259/" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin']" /></a></div>
         </div>
-        <div class="grid linea">
+        <div class="grid linea marginTablet">
             <p>GitHub:</p>
             <div class="gitHub"><a href="https://github.com/trontzal" target="_blank"><font-awesome-icon :icon="['fab', 'github']" /></a></div>
         </div>
-        <div>
+        <div class="marginTablet">
             <p>Gmail:</p>
             <p class="gmail"><a class="link" :href="'mailto:gonzalo.lekunberri@gmail.com'">gonzalo.lekunberri@gmail.com</a></p>
         </div>
@@ -41,5 +41,17 @@
     display: flex;
     flex-direction: column;
     gap: 2rem;
+  }
+
+  @media  screen and (max-width:900px) {
+    .marginTablet{
+        margin-left: 4rem;
+    }
+  }
+
+  @media  screen and (max-width:767px) {
+    .marginTablet{
+        margin-left: 0;
+    }
   }
 </style>
