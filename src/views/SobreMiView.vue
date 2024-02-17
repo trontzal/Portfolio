205908b4bed3ccf0c2450999d5419dc303f075be
Gonzalo Lecumberri <trontzal@gmail.com>
2024-02-17 13:21:03 +0100

    Sobre mi flex arreglado creo

--- src/views/SobreMiView.vue
+++ src/views/SobreMiView.vue
@@ -5,22 +5,24 @@
       <h2 class="sombraTitulo">SOBRE MI</h2>
     </div>
     <div class="contenidosSobreMi">
-
-      <cuadradoComp id="DatosPersonales">
-        <datosPersonales class="dentroCuadrado datosPersonales"></datosPersonales>
-      </cuadradoComp>
-
-
-      <div class="juntosFlex">
-        <cuadradoComp>
-          <stack class="dentroCuadrado cuadradoStack"></stack>
+      <div class="datosPersonalesYStack">
+        <cuadradoComp id="DatosPersonales">
+          <datosPersonales class="dentroCuadrado datosPersonales"></datosPersonales>
         </cuadradoComp>
 
 
+        <cuadradoComp>
+          <stack class="dentroCuadrado cuadradoStack"></stack>
+        </cuadradoComp>
+      </div>
+
+
+      <div class="experienciaDiv">
         <cuadradoComp class="espacioAbajo">
           <experiencia class="dentroCuadrado flexExperiencia"></experiencia>
         </cuadradoComp>
       </div>
+
 
     </div>
   </div>
@@ -32,8 +34,8 @@ import datosPersonales from "@/components/SobreMi/datosPersonales.vue"
 import stack from "@/components/SobreMi/stack.vue"
 import experiencia from "@/components/SobreMi/experiencia.vue"
 
-export default{
-  components:{
+export default {
+  components: {
     cuadradoComp,
     datosPersonales,
     stack,
@@ -43,71 +45,86 @@ export default{
 </script>
 
 <style>
+#SobreMiView {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+}
 
-  #SobreMiView{
+
+.contenidosSobreMi {
+  display: flex;
+  flex-direction: column;
+}
+
+
+.datosPersonalesYStack{
+  display: flex;
+  width: 100%;
+}
+
+
+
+
+.dentroCuadrado {
+  color: white;
+  padding: 1rem;
+}
+
+.datosPersonales {
+  width: 20rem;
+}
+
+.fixed {
+  position: fixed;
+}
+
+.linea {
+  border-bottom: 1px solid rgba(128, 128, 128, 0.295);
+  padding-bottom: 0.3rem;
+}
+
+.linkedin {
+  color: blue;
+  font-size: 24px;
+}
+
+.gitHub {
+  color: white;
+  font-size: 24px;
+}
+
+.gmail {
+  margin-top: 1rem;
+}
+
+@media screen and (max-width:900px) {
+  .contenidosSobreMi {
     display: flex;
-    width: 100%;
     flex-direction: column;
   }
 
+  .datosPersonalesYStack{
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
 
-  .contenidosSobreMi{
-    display: flex;
-    flex-direction: row;
+  .sombraTitulo {
+    font-size: 3rem;
   }
 
-
-  .dentroCuadrado{
-    color: white;
-    padding: 1rem;
+  #DatosPersonales {
+    width: 100%;
   }
 
-  .datosPersonales{
-    width: 20rem;
+  .datosPersonales {
+    width: 90%;
   }
 
-  .fixed{
-    position: fixed;
+  .espacioAbajo {
+    padding-bottom: 6rem;
   }
 
-
-
-  .linea{
-    border-bottom: 1px solid rgba(128, 128, 128, 0.295);
-    padding-bottom: 0.3rem;
-  }
-
-  .linkedin{
-    color: blue;
-    font-size: 24px;
-  }
-
-  .gitHub{
-    color:white;
-    font-size: 24px;
-  }
-
-  .gmail{
-    margin-top: 1rem;
-  }
-
-  @media  screen and (max-width:900px) {
-      .contenidosSobreMi{
-        display: flex;
-        flex-direction: column;
-      }
-      .sombraTitulo{
-        font-size: 3rem;
-      }
-      #DatosPersonales{
-        width: 100%;
-      }
-      .datosPersonales{
-        width: 90%;
-      }
-      .espacioAbajo{
-        padding-bottom: 6rem;
-      }
-
-    }
+}
 </style>
